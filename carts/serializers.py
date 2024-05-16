456ddcc4a867635aa749db202fa0d5e7dc26f7ae
Nudlik <gegorp@mail.ru>
2024-05-16 19:32:12 +0700

    расширил описание на получение карт_айтема поподробней

---
+++ carts/serializers.py
@@ -0,0 +1,56 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from carts.models import Cart
+from products.serializers import ProductSerializer
+
+
+class CartListSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True, many=False)
+    price = serializers.SerializerMethodField()
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = [
+            'product',
+            'quantity',
+            'price',
+            'total_price',
+            'is_discount',
+        ]
+
+    def get_product(self, obj):
+        return obj.product.name
+
+    def get_price(self, obj):
+        if obj.is_discount:
+            return obj.quantity * obj.product.discount
+        return obj.quantity * obj.product.price
+
+    def get_total_price(self, obj):
+        if obj.is_discount:
+            return obj.product.discount
+        return obj.product.price
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = [
+            'product',
+            'quantity',
+        ]
+
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                _('Количество должно быть больше нуля')
+            )
+        return value
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        instance = self.Meta.model.objects.create(**validated_data)
+        instance.save()
+        return instance
--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from carts.models import Cart
+from carts.serializers import CartListSerializer, CartSerializer
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+    queryset = Cart.objects.all()
+    choice_serializer = {
+        'list': CartListSerializer,
+        'retrieve': CartListSerializer,
+    }
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        return self.choice_serializer.get(self.action, self.serializer_class)
