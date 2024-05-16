d47f7aa30e48dd36580a910b7c2ec6c27154c34d
Nudlik <gegorp@mail.ru>
2024-05-16 15:07:31 +0700

    написал вьюсет для категорий

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from categories.models import Category
+from categories.serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
