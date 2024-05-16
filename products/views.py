88277815f193cbe771a38c40ff50220db6bb0781
Nudlik <gegorp@mail.ru>
2024-05-16 16:26:06 +0700

    добавил права на редактирования только своих товаров

--- products/views.py
+++ products/views.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from products.models import Product
+from products.permissions import IsOwner
 from products.serializers import ProductSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    perms_methods = {
+        'list': [AllowAny],
+        'update': [IsOwner | IsAdminUser],
+        'partial_update': [IsOwner | IsAdminUser],
+        'destroy': [IsOwner | IsAdminUser],
+    }
+
+    def get_permissions(self):
+        self.permission_classes = self.perms_methods.get(self.action, self.permission_classes)
+        return [permission() for permission in self.permission_classes]
