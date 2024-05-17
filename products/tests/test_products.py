e880c048515c21f93f23f5cf5668993f494492ab
Nudlik <gegorp@mail.ru>
2024-05-17 21:59:06 +0700

    добавил еще пару тестов для приличия

--- products/tests/test_products.py
+++ products/tests/test_products.py
@@ -16,8 +16,30 @@ class ProductCRUDTestCase(TestCase):
         self.total_users = 3
 
         self.category1 = Category.objects.create(name='category1')
+        self.category2 = Category.objects.create(name='category2')
 
-        self.total_products = 0
+        self.product1 = Product.objects.create(
+            name='product1',
+            price=123,
+            discount=100,
+            quantity=10,
+            description='description1',
+            category=self.category1,
+            owner=self.user1,
+        )
+        self.product2 = Product.objects.create(
+            name='product2',
+            price=2,
+            discount=1,
+            quantity=1,
+            description='description2',
+            category=self.category1,
+            owner=self.user2,
+        )
+        self.product1.save()
+        self.product2.save()
+
+        self.total_products = 2
 
     def test_create_product(self):
         data = {
@@ -26,7 +48,7 @@ class ProductCRUDTestCase(TestCase):
             'discount': '50.00',
             'quantity': 10,
             'description': 'description1',
-            'category': 1,
+            'category': self.category1.id,
         }
         self.client.force_login(self.user1)
         url = reverse('products-list')
@@ -34,3 +56,47 @@ class ProductCRUDTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Product.objects.count(), self.total_products + 1)
         self.assertEqual(Product.objects.last().owner, self.user1)
+
+    def test_list_products(self):
+        url = reverse('products-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), self.total_products)
+
+    def test_delete_product(self):
+        self.client.force_login(self.user1)
+        url = reverse('products-detail', args=[self.product1.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Product.objects.count(), self.total_products - 1)
+
+    def test_delete_product_not_owner(self):
+        self.client.force_login(self.user1)
+        url = reverse('products-detail', args=[self.product2.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Product.objects.count(), self.total_products)
+
+    def test_update_product(self):
+        data = {
+            'name': 'change_product1',
+            'price': '1000',
+            'discount': '500.00',
+            'quantity': 100,
+            'description': 'change_description1',
+            'category': self.category2.id,
+        }
+        self.client.force_login(self.user1)
+        self.product1.refresh_from_db()
+        url = reverse('products-detail', args=[self.product1.id])
+        response = self.client.patch(url, data, 'application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Product.objects.count(), self.total_products)
+
+        change_product = Product.objects.get(id=self.product1.id)
+        self.assertEqual(change_product.name, 'change_product1')
+        self.assertEqual(change_product.price, 1000)
+        self.assertEqual(change_product.discount, 500.00)
+        self.assertEqual(change_product.quantity, 100)
+        self.assertEqual(change_product.description, 'change_description1')
+        self.assertEqual(change_product.category.id, self.category2.id)
